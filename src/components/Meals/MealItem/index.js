f27cd10e08a0b1ce2e6d9b385704bc9867b88829
Louis Alemar <louisalemar@gmail.com>
2023-10-10 13:11:35 -0400

    add meal components

---
+++ src/components/Meals/MealItem/index.js
@@ -0,0 +1,18 @@
+import classes from "./MealItem.module.scss";
+
+const MealItem = ({ name, description, price }) => {
+  const mealPrice = `$${price.toFixed(2)}`;
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{name}</h3>
+        <div className={classes.description}>{description}</div>
+        <div className={classes.price}>{mealPrice}</div>
+      </div>
+      <div></div>
+    </li>
+  );
+};
+
+export default MealItem;
